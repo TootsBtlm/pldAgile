b12d195267084f9f3096112b99b12d49473940b2
Aurelien Carpentier <aurecarp@live.fr>
2020-10-21 14:07:18 +0200

    Adding button to load map

--- src/vue/Main.java
+++ src/vue/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import modele.EnsembleRequete;
 import modele.Lecteur;
 import modele.Plan;
 
@@ -20,13 +21,18 @@ public class Main extends Application {
 			Plan nouveauPlan = l.LirePlan("ressources/largeMap.xml");
 			
 			UiController ui = loader.getController();
+			ui.setStage(primaryStage);
 			
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(new Scene(root));
 			primaryStage.setResizable(false);
 			primaryStage.show();
 			
-			ui.drawPlan(nouveauPlan);
+//			ui.drawPlan(nouveauPlan);
+//			
+//			EnsembleRequete r = l.LireRequete("ressources/requestsLarge7.xml", nouveauPlan);
+//			
+//			ui.drawRequests(r);
 			
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/vue/UiController.java
+++ src/vue/UiController.java
@@ -1,11 +1,19 @@
 package vue;
 
+import java.io.File;
+
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
 import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import modele.Depot;
+import modele.EnsembleRequete;
 import modele.Intersection;
+import modele.Lecteur;
 import modele.Plan;
+import modele.Requete;
 import modele.Segment;
 
 public class UiController {
@@ -15,11 +23,24 @@ public class UiController {
 	private Canvas planCanvas;
 
 	@FXML
-	private Button button1;
+	private Button btnOuvrirPlan;
+	
+	private Stage stage;
 
 
 	@FXML
 	public void initialize() {
+	}
+	
+	@FXML
+	public void choisirFichierPlan() {
+		FileChooser fileChooser = new FileChooser();
+		File file = fileChooser.showOpenDialog(this.stage);
+		String path = file.getPath();
+		System.out.println(path);
+		Lecteur lecteur = new Lecteur();
+		Plan p = lecteur.LirePlan(path);
+		this.drawPlan(p);
 	}
 
 
@@ -34,6 +55,7 @@ public class UiController {
 
 	public void drawPlan(Plan plan) {
 		var gc = this.planCanvas.getGraphicsContext2D();
+		gc.clearRect(0, 0, this.planCanvas.getWidth(), this.planCanvas.getHeight());
 		//		gc.beginPath();
 		//		gc.moveTo(this.planCanvas.getWidth(), 0);
 		//		gc.lineTo(30.5, 30.5);
@@ -51,7 +73,6 @@ public class UiController {
 			double y = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (plan.getIntersection().get(i).getLatitude() - latitudeMin) / (latitudeMax - latitudeMin)); 
 			gc.setFill(Color.RED);
 			gc.fillRect(x-2, y-2, 2, 2);
-			System.out.println("x:" + x + ", y: " + y);
 		}
 		
 		for(int i = 0; i < plan.getSegment().size(); i++) {
@@ -71,5 +92,50 @@ public class UiController {
 			gc.stroke();
 
 		}
+	}
+	
+	public void drawRequests(EnsembleRequete er) {
+		
+		double latitudeMin = 45.727352;
+		double latitudeMax = 45.780518;
+
+		double longitudeMin = 4.8314367;
+		double longitudeMax = 4.9075384;
+		
+		double rayonCercle = 20.0;
+		
+		var gc = this.planCanvas.getGraphicsContext2D();
+		Depot depot = er.getLieuDepart();
+		
+		double depotX = this.planCanvas.getWidth() * (depot.getPointDeDepart().getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
+		double depotY = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (depot.getPointDeDepart().getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
+		gc.setFill(Color.ORANGE);
+		gc.fillOval(depotX - rayonCercle/2, depotY - rayonCercle/2, rayonCercle, rayonCercle);
+		
+		for(int i = 0; i < er.getListeRequete().size(); i++) {
+			Requete req = er.getListeRequete().get(i);
+			Intersection pRecup = req.getPointDeRecuperation();
+			Intersection pLiv = req.getPointDeLivraison();
+			
+			System.out.println(pLiv);
+			
+			double recupX = this.planCanvas.getWidth() * (pRecup.getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
+			double recupY = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (pRecup.getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
+			
+			double livX = this.planCanvas.getWidth() * (pLiv.getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
+			double livY = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (pLiv.getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
+			
+			
+			
+			gc.setFill(Color.BLUE);
+			gc.fillOval(recupX - rayonCercle/2, recupY - rayonCercle/2, rayonCercle, rayonCercle);
+			
+			gc.setFill(Color.YELLOW);
+			gc.fillOval(livX - rayonCercle/2, livY - rayonCercle/2, rayonCercle, rayonCercle);
+		}
+	}
+	
+	public void setStage(Stage stage) {
+		this.stage = stage;
 	}
 }
