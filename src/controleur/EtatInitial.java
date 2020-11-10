eeac224e0e618d309b69189e4538062f87354d68
jj1999 <jjacques.meldrum@gmail.com>
2020-11-10 10:36:26 +0100

    Javadoc avancement

--- src/controleur/EtatInitial.java
+++ src/controleur/EtatInitial.java
@@ -7,17 +7,27 @@ import vue.VueGraphique;
 
 /**
  * 
- * @author Mario
- * Représente l'état initial de l'application, qui hérite de l'état général
+ * @author Mario et Jean-Jacques
+ * Représente l'état initial de l'application, qui hérite de la classe état
  * 
  */
 
 public class EtatInitial extends Etat {
 
+	/**
+	 * 
+	 * @param interfaceController
+	 * Prend l'objet interface controleur de la classe InterfaceControleur pour modifier les attributs de l'objet interface controleur existant
+	 * 
+	 */
 	public EtatInitial(InterfaceController interfaceController) {
 		super(interfaceController);
 	}
-
+	/**
+	 * 
+	 * Fait appel à chargerFichierPlan de l'objet interface controleur 
+	 * 
+	 */
 	public void chargerFichierPlan() {
 		
 		this.interfaceController.chargerFichierPlan();
