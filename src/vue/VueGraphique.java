34083f707579c4e005881feebf4c53a3163c68d5
Aurelien Carpentier <aurecarp@live.fr>
2020-11-13 14:15:05 +0100

    Javadoc and removing all commented lines

--- src/vue/VueGraphique.java
+++ src/vue/VueGraphique.java
@@ -1,9 +1,7 @@
 package vue;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -47,6 +45,9 @@ public class VueGraphique {
 		return(this.plan);
 	}
 
+	/**
+	 * Méthode permettant de dessiner le plan sur l'écran
+	 */
 	public void drawPlan() {
 		requetes.clear();
 		intersectionPane.getChildren().clear();
@@ -55,10 +56,6 @@ public class VueGraphique {
 		gc.clearRect(0, 0, this.planCanvas.getWidth(), this.planCanvas.getHeight());
 		var gc2 = this.itineraireCanvas.getGraphicsContext2D();
 		gc2.clearRect(0, 0, this.itineraireCanvas.getWidth(), this.itineraireCanvas.getHeight());
-		//		gc.beginPath();
-		//		gc.moveTo(this.this.planCanvas.getWidth(), 0);
-		//		gc.lineTo(30.5, 30.5);
-		//		gc.stroke();
 
 		double latitudeMin = this.plan.latitudeMin();
 		double latitudeMax = this.plan.latitudeMax();
@@ -68,14 +65,6 @@ public class VueGraphique {
 
 		System.out.println(latitudeMax + " " + latitudeMin);
 		System.out.println(longitudeMax + " " + longitudeMin);
-
-
-//		for(int i = 0; i < this.plan.getIntersection().size(); i++) {
-//			double x =  this.planCanvas.getWidth() * (this.plan.getIntersection().get(i).getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
-//			double y = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (this.plan.getIntersection().get(i).getLatitude() - latitudeMin) / (latitudeMax - latitudeMin)); 
-//			gc.setFill(Color.RED);
-//			gc.fillRect(x-2, y-2, 2, 2);
-//		}
 
 		for(int i = 0; i < this.plan.getSegment().size(); i++) {
 			Segment s = this.plan.getSegment().get(i);
@@ -98,7 +87,6 @@ public class VueGraphique {
 				this.intersectionPane.getChildren().add(c1);
 			}
 
-
 			Circle c2 = new Circle(8.0);
 			c2.setCenterX(x2);
 			c2.setCenterY(y2);
@@ -109,8 +97,6 @@ public class VueGraphique {
 				this.intersectionPane.getChildren().add(c2);
 			}
 			
-			
-
 			gc.beginPath();
 			gc.moveTo(x1, y1);
 			gc.setLineWidth(1.0);
@@ -119,10 +105,13 @@ public class VueGraphique {
 			gc.lineTo(x2, y2);
 			gc.stroke();
 		}
-		//System.out.println("NODE : " + nodeLinkedToIntersection);
 		mouseEvents.setNodeLinkedToIntersection(nodeLinkedToIntersection);
 	}
 
+	/**
+	 * Méthode permettant de dessiner les requêtes sur l'écran
+	 * @param er Ensemble des requêtes à dessiner
+	 */
 	public void drawRequests(EnsembleRequete er) {
 
 		if(requetes.size() > 0) {
@@ -130,39 +119,15 @@ public class VueGraphique {
 
 				Circle node = (Circle)(requetes.get(i));
 				node.setFill(Color.rgb(0,0,0,0));
-				//intersectionPane.getChildren().remove(requetes.get(i));
 			}
 			requetes.clear();
 		}
-		//		requetePane.getChildren().clear();
-		//		nodeLinkedToIntersection.clear();
 		var gc2 = this.itineraireCanvas.getGraphicsContext2D();
 		gc2.clearRect(0, 0, this.itineraireCanvas.getWidth(), this.itineraireCanvas.getHeight());
 
-		double latitudeMin = this.plan.latitudeMin();
-		double latitudeMax = this.plan.latitudeMax();
-
-		double longitudeMin = this.plan.longitudeMin();
-		double longitudeMax = this.plan.longitudeMax();
-
-		double rayonCercle = 8.0;
-
-		//		drawPlan();
 		Depot depot = er.getLieuDepart();
 
-		double depotX = this.intersectionPane.getWidth() * (depot.getPointDeDepart().getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
-		double depotY = this.intersectionPane.getHeight() - (this.intersectionPane.getHeight() * (depot.getPointDeDepart().getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
 		Circle depotC = (Circle)(nodeLinkedToIntersection.inverse().get(depot.getPointDeDepart()));
-		//		File imgDepot = new File("ressources/img/warehouse.png");
-		//		ImageView warehouse = new ImageView();
-		//		warehouse.setImage(new Image(imgDepot.toURI().toString()));
-		//		
-		//		warehouse.setFitWidth(32);
-		//		warehouse.setX(depotX - 16);
-		//		warehouse.setY(depotY - 16);
-		//		warehouse.setPreserveRatio(true);
-		//		depotC.setCenterX(depotX);
-		//		depotC.setCenterY(depotY);
 		depotC.setFill(Color.ORANGE);
 		depotC.toFront();
 		this.requetes.add(depotC);
@@ -174,40 +139,29 @@ public class VueGraphique {
 
 			System.out.println(pLiv);
 
-			double recupX = this.intersectionPane.getWidth() * (pRecup.getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
-			double recupY = this.intersectionPane.getHeight() - (this.intersectionPane.getHeight() * (pRecup.getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
-
 			Circle recupC = (Circle)(nodeLinkedToIntersection.inverse().get(pRecup));
-			//			recupC.setCenterX(recupX);
-			//			recupC.setCenterY(recupY);
 			recupC.setFill(Color.YELLOW);
 			recupC.toFront();
 			this.requetes.add(recupC);
 
-			double livX = this.intersectionPane.getWidth() * (pLiv.getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
-			double livY = this.intersectionPane.getHeight() - (this.intersectionPane.getHeight() * (pLiv.getLatitude() - latitudeMin) / (latitudeMax - latitudeMin));
-
 			Circle livC = (Circle)(nodeLinkedToIntersection.inverse().get(pLiv));
-			//			livC.setCenterX(livX);
-			//			livC.setCenterY(livY);
 			livC.setFill(Color.BLUE);
 			livC.toFront();
 			this.requetes.add(livC);
 		}
-		//intersectionPane.getChildren().addAll(requetes);
-		//		requetePane.getChildren().add(warehouse);
 		for(int i = 0; i < this.requetes.size(); i++) {
 			mouseEvents.requeteCliquable(requetes.get(i));
 		}
 	}
 
+	/**
+	 * Méthode permettant de dessiner sur l'écran l'itinéraire de livraison
+	 * @param livraison L'itinéraire à dessiner
+	 */
 	public void drawItineraire(Livraison livraison) {
 		var gc = this.itineraireCanvas.getGraphicsContext2D();
-
-//		gc.setFill(Color.RED);
 		
 		gc.clearRect(0, 0, this.itineraireCanvas.getWidth(), this.itineraireCanvas.getHeight());
-
 
 		double latitudeMin = this.plan.latitudeMin();
 		double latitudeMax = this.plan.latitudeMax();
@@ -216,12 +170,10 @@ public class VueGraphique {
 		double longitudeMax = this.plan.longitudeMax();
 		
 		int sizeItineraire = livraison.getListeItineraires().size();
-		
 
 		for(int i = 0; i < sizeItineraire; i++) {
 			
 			double luminosite = 1.0 - ((double)i / (double)sizeItineraire);
-//			double color = (double)i / (double)sizeItineraire;
 			gc.setFill(Color.hsb(240, 1.0, luminosite));
 			Itineraire itineraire = livraison.getListeItineraires().get(i);
 			for(int j = 0; j < itineraire.getListeIntersections().size() - 1; j++) {
@@ -233,7 +185,6 @@ public class VueGraphique {
 
 				double x2 = this.planCanvas.getWidth() * (i2.getLongitude() - longitudeMin) / (longitudeMax - longitudeMin); 
 				double y2 = this.planCanvas.getHeight() - (this.planCanvas.getHeight() * (i2.getLatitude() - latitudeMin) / (latitudeMax - latitudeMin)); 
-
 
 				gc.beginPath();
 				gc.moveTo(x1, y1);
